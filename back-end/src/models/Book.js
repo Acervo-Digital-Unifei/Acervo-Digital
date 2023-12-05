0e741f2e4454266f2b34b97306b38734efe0b7be
Diogo <diogohandrad@gmail.com>
2023-12-05 20:19:34 -0300

    Adicionadas rotas GET para os livros

--- back-end/src/models/Book.js
+++ back-end/src/models/Book.js
@@ -1,12 +1,14 @@
 import { Schema, model } from 'mongoose';
 
-const Book = model('Book', new Schema({
+const BookSchema = new Schema({
     title: {type: String, required: true},
     author: {type: String, required: true},
     publisher: {type: String, required: true},
     ISBN: {type: String, default: null},
     thumbnail: {type: String, default: null},
     price: {type: Number, required: true},
-}), 'books');
+});
+
+const Book = model('Book', BookSchema, 'books');
 
 export default Book;
