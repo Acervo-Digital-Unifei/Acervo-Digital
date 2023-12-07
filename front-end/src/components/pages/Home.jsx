7aedd7c2945cbc689701221fb78872467399e6fd
Nikolas Pires <nikolasspires@gmail.com>
2023-12-07 15:01:18 -0300

    h1 perdido na home

--- front-end/src/components/pages/Home.jsx
+++ front-end/src/components/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <Container customClass='background-img'>
             <section>
-                <h1>Home</h1>
+               
             </section>
         </Container>
     )
