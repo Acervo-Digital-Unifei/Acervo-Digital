3287d5d5abcd7247d9b2b195f5e3c4abd191de65
Nikolas Pires <nikolasspires@gmail.com>
2023-12-07 11:08:59 -0300

    row gap

--- front-end/src/components/pages/Livros.jsx
+++ front-end/src/components/pages/Livros.jsx
@@ -10,6 +10,9 @@ import Card  from '../cards/Card'
 
 export default function Livros() {
     const [books, setBooks] = useState([])
+    const [search, setSearch] = useState('');
+    const [found, setFound] = useState(false);
+
     const getBooks = async () => {
         const result = await axios.get("http://localhost:3000/book");
         const books = result.data.books;
@@ -21,27 +24,30 @@ export default function Livros() {
 useEffect(() => {
 
         getBooks()
+        
     }, []);
     
-
+   
     return( 
         <Container customClass='backgroundStandartG'>
             <h2></h2>
             <section className={styles.livros_busca}>
                 <div className={styles.buscador}>
-                    <Input name="Busca" type="text" placeholder="Buscar" customClass="inputBusca" />  
+                    <Input name="Busca" type="text" placeholder="Buscar" customClass="inputBusca"/>  
                 </div>
                 <div className={styles.grid}>
                     {
+                        
                         books.map((livro) => (<Card title={(livro.title)} author={livro.author} publisher={livro.publisher}
                         price={livro.price}
                         thumbnail={livro.thumbnail}
                         />))
                     }
+                    {
+                        
+                    }
                 </div>
-               {/* <Card title={(livro.title)} author={livro.author} publisher={livro.publisher}
-               price={livro.price}
-               /> */}
+               
                     
             </section>
         </Container>
