1251fda58c4e33b665e80b9d38bb46452e5dfc71
pedropmoreira <pedropscontatoprof@gmail.com>
2023-12-07 08:01:38 -0300

    PUSH- paginalivro-semcss-rotasapp

--- front-end/src/components/pages/PaginaLivro.jsx
+++ front-end/src/components/pages/PaginaLivro.jsx
@@ -1,32 +1,60 @@
 import Container from "../layouts/Container";
-import * as styles from './PaginaLivro.module.css';
+import styles from './AtualizarLivro.module.css'
+import Input from "../Input";
 import Button from "../Button";
-import { useContext } from "react";
 import { UserContext } from "../../App";
-
-import { useRef, useState } from 'react'
+import { useState } from "react";
 import { Navigate } from "react-router-dom";
+import { useContext } from "react";
 
 export default function PaginaLivro() {
-    //states 
-    const [bookTitle, setBookTitle] = useState("O homem e seus símbolos");
-    const [author, setAuthor] = useState("Carl G. Jung");
-    const [publisher, setPublisher] = useState("HarperCollins Brasil");
-    const { user, setUser } = useContext(UserContext);
     
-    const buttonAdicionar = ()=>{
-        Navigate("/carrinho");
-    }
-    const buttonAtualizar = ()=>{
-        if(user.privilege){
-            Navigate("/atualizarlivro");}
-        else{alert("Você não tem permissao pra isso!")};
-        
+    const [nome, setNome] = useState("");
+    const [preco, setPreco] = useState("");
+    const [editora, setEditora] = useState("");
+    const [autor, setAutor] = useState("");
+    
+    const { user, setUser } = useContext(UserContext);
+
+    const goToAtualiza = ()=>{
+        if (user !== null && user.privilege === 'admin') {
+            Navigate("/atualizalivro");
+          } else {
+            alert("Você não tem permissão para isso!");
+          }
+        }
+
+    const addCarrinho =()=> { 
+        if (user !== null) {
+            Navigate("/carrinho");
+          } else {
+            alert("Voce deve estar logado para isso!");
+            Navigate("/login");
+          }
+
     }
     return (
     <Container customClass='backgroundStandart'>
-        
-        
+        <section className={styles.atualiza}>
+            <h2></h2>
+            <form action="">
+                <div className={styles.imgAtualiza}>
+
+                </div>
+                <div className={styles.inputsLivro}>
+                    <h3>{nome}Nome livro</h3>
+                    <p>{preco}preço</p>
+                    <br/>
+                    <p>{editora}Editora</p>
+                    <p>{autor}Autor</p>
+                   
+                   '' <div className={styles.submit}>
+                        <Button text="Carrinho" customClass="marginless" onClick={addCarrinho} />
+                        <Button text="Atualiza" customClass="marginless" onClick={goToAtualiza} />
+                    </div>
+                </div>
+            </form>
+        </section>
     </Container>
 )
 }
