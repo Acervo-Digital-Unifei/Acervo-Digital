5b030998a60811b5aef59bb8f7b183f5195a7f29
Diogo <diogohandrad@gmail.com>
2023-12-07 07:59:55 -0300

    Alterado toast para canto superior direito da tela

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
-    <ToastContainer floatingTime={5000} position='top-left'/>
+    <ToastContainer floatingTime={5000} position='top-right'/>
   </>,
 )
 
